3a92c41ac29dfcf4859f482e72139ce47249bd2f
Tom Straub <tstraub@hashicorp.com>
2020-08-20 13:53:13 -0500

    Remove unneeded submodule

--- modules/workspace/outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "workspaces" {
-#   description = ""
-#   value = [for ws in tfe_workspace.ws : ws.id]
-# }
-
-# output "teams" {
-#   description = ""
-#   value = [for ta in tfe_team_access.access : ta.id]
-# }
